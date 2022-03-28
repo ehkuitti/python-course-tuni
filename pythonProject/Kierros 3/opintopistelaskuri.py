749f2187b49cd081e10dfd3d605f5bbca23f832f
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-03-28 12:47:25 +0300

    Update opintopistelaskuri.py
    Output-formatointia vaille valmis

--- pythonProject/Kierros 3/opintopistelaskuri.py
+++ pythonProject/Kierros 3/opintopistelaskuri.py
@@ -10,61 +10,54 @@ Opiskelijanumero: 150541820
 
 def main():
     # Flag-tyyliset muuttujat
-    i = 1
-    jarjestysnumero = 1
-    onko_kuukausia_vahintaan_kaksi = False
-    onko_vastaus_ollut_nolla = False
-    onko_kuukausia_liian_vahan = False
+    kierrosluku = 1
+    onko_opintoja_liian_vahan = False
 
     # Arvojen säilyttäjät
+    edellisen_kuukauden_pisteet = 0.0
+    pisteet_yhteensa = 0.0
+    nykyisen_kuukauden_pisteet = 0.0
+    opintopisteiden_maara_tassa_kuussa = 0.0
     keskiarvo = 0.0
-    nykyinen_kuukausi = 0.0
-    opintopisteiden_maara_kuussa = 0.0
-    opintopisteet_yhteensa = 0
-    seuraava_kuukausi = 1.0
 
     str_kuukaudet = input("Enter the number of months: ")
     kuukausien_maara = int(str_kuukaudet)
-    if kuukausien_maara >= 2.0:
-        onko_kuukausia_vahintaan_kaksi = True
-    else:
-        onko_kuukausia_liian_vahan = True
 
-    for i in range(0, kuukausien_maara):
+    for i in range(1, kuukausien_maara+1):
 
-        print("Enter the number of credits in month ", jarjestysnumero, ":", sep="", end=' ')
+        print("Enter the number of credits in month ", kierrosluku, ":", sep="", end=' ')
         str_opintopisteet = input("")
-        opintopisteiden_maara_kuussa = int(str_opintopisteet)
-        if opintopisteiden_maara_kuussa == 0:
-            onko_vastaus_ollut_nolla = True
+        opintopisteiden_maara_tassa_kuussa = int(str_opintopisteet)
+        pisteet_yhteensa += opintopisteiden_maara_tassa_kuussa
 
-        if onko_kuukausia_vahintaan_kaksi and (nykyinen_kuukausi >= 1):
-            if i % 2 == 0:
-                seuraava_kuukausi = opintopisteiden_maara_kuussa
-                opintopisteet_yhteensa += seuraava_kuukausi
-            else:
-                nykyinen_kuukausi = opintopisteiden_maara_kuussa
-                opintopisteet_yhteensa += nykyinen_kuukausi
+        if kierrosluku == 1:
+            edellisen_kuukauden_pisteet = opintopisteiden_maara_tassa_kuussa
+            nykyisen_kuukauden_pisteet = opintopisteiden_maara_tassa_kuussa
 
-            if nykyinen_kuukausi + seuraava_kuukausi == 0:
-                print("You did have too many study breaks!")
-                break
+        elif kierrosluku == 2:
+            nykyisen_kuukauden_pisteet = opintopisteiden_maara_tassa_kuussa
+
+        else:
+            edellisen_kuukauden_pisteet = nykyisen_kuukauden_pisteet
+            nykyisen_kuukauden_pisteet = opintopisteiden_maara_tassa_kuussa
+
+        if kierrosluku != 1 and edellisen_kuukauden_pisteet + nykyisen_kuukauden_pisteet == 0:
+            print("You did have too many study breaks!")
+            break
 
         i += 1
-        nykyinen_kuukausi += 1
-        seuraava_kuukausi += 1
-        jarjestysnumero += 1
+        kierrosluku += 1
 
-    keskiarvo = opintopisteet_yhteensa / i
+    keskiarvo = pisteet_yhteensa / kuukausien_maara
 
-    if onko_kuukausia_liian_vahan:
-        print(f'\nYour monthly credit point average {keskiarvo:.1f} does not classify you as a full time student.')
-
-    elif onko_vastaus_ollut_nolla:
-        print(f'\nYour monthly credit point average {keskiarvo:.1f} does not classify you as a full time student.')
+    if keskiarvo < 5.0:
+        print("\n")
+        print(f'Your monthly credit point average {keskiarvo:.1f} does not classify you as a full time '
+              f'student.')
 
     else:
-        print(f'\nYou are a full time student and your monthly credit point average is {keskiarvo:.1f}')
+        print("\n")
+        print(f'\nYou are a full time student and your monthly credit point average is {keskiarvo:.1f}.')
 
 
 if __name__ == "__main__":
