4f7ff42f87213a859d363f98674250f72abb1cea
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-03-30 16:44:43 +0300

    yogi_bear.py korjattu

--- pythonProject/Kierros 3/yogi_bear.py
+++ pythonProject/Kierros 3/yogi_bear.py
@@ -8,34 +8,34 @@ Koodi tulostaa kappaletta eri parametreillä.
 """
 
 
-def repeat_name(animal_name, count = 0):
+def repeat_name(animal_name, count):
     """Toistaa eläimen nimeä"""
-    print(animal_name, ", ", animal_name, sep="", end="")
+
+    if count == 1:
+        print(animal_name, ", ", animal_name, " Bear", sep="")
+
+    elif count == 3:
+        print(animal_name, ", ", animal_name, " Bear", sep="")
+        print(animal_name, ", ", animal_name, " Bear", sep="")
+        print(animal_name, ", ", animal_name, " Bear", sep="")
 
 
 def verse(clause, name):
     """Toistaa säkeistöä käyttäen mainilta tulevia parametrejä
     ja kutsuen repeat_namea eläinten nimien tulostukseen"""
     print(clause)
-    repeat_name(name)
-    print("\n", clause, sep="")
-    repeat_name(name)
-    print(" Bear")
-    repeat_name(name)
-    print(" Bear")
-    repeat_name(name)
-    print(" Bear")
+    print(name, ", ", name, sep="")
     print(clause)
-    repeat_name(name)
-    if name == "Cindy":
-        print(" Bear", end="")
-    else:
-        print(" Bear", "\n", sep="")
+    repeat_name(name, 3)
+    print(clause)
+    repeat_name(name, 1)
 
 
 def main():
     verse("I know someone you don't know", "Yogi")
+    print("")
     verse("Yogi has a best friend too", "Boo Boo")
+    print("")
     verse("Yogi has a sweet girlfriend", "Cindy")
 
 
