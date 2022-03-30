b85cfad41dc9bd1ef495e6d0c866e6bcb101478f
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-03-30 07:49:49 +0300

    Luotu pohja
    Pohjatiedosto sisältää jokaiseen tiedostoon tarvittavat pakolliset koodipätkät / tekstit

---
+++ pythonProject/Pohjat/koodipohja.py
@@ -0,0 +1,15 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+"""
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
+
