db99fb435147bf2ddb7a93a427b78bbdfd9e2510
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-02 13:53:16 +0300

    Ruutu_2 valmis, kolmion tiedosto luotu

---
+++ pythonProject/Kierros 3/kolmio.py
@@ -0,0 +1,21 @@
+"""
+Ohjelmointi 1 / Programming 1
+Trangle's Area when the Sides Are Known
+"""
+
+from math import sqrt
+
+def area(sivu1, sivu2, sivu3):
+    pass
+
+
+def main():
+    line = input("Enter the length of the first side: ")
+    line = input("Enter the length of the second side: ")
+    line = input("Enter the length of the third side: ")
+
+    print("The triangle's area is ")
+
+
+if __name__ == "__main__":
+    main()
