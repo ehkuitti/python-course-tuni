9013cbc67aaaa83c60a5ecb2ec29dba788b6993b
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-05 12:30:15 +0300

    geometriset_kuviot.py neliö valmis

---
+++ pythonProject/Kierros 4/geometriset_kuviot.py
@@ -0,0 +1,79 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+Ohjelma laskee pinta-aloja erilaisille kuvioille käyttäjän syötteen perusteella.
+"""
+
+from math import pi
+from math import pow
+
+
+def is_value_unsigned(float_value):
+    return float_value <= 0
+
+
+def count_values_for_a_rectangle():
+    pass
+
+
+def count_values_for_a_square():
+    """Laskee arvoja neliölle, ei ota vastaan parametreja."""
+
+    circumference = 0.0
+    is_value_valid = False
+    side_1 = 0.0
+    surface_area = 0.0
+    bool_value_storage = False
+
+    while not is_value_valid:
+
+        side_1 = float(input("Enter the lenght of the square's side: "))
+        bool_value_storage = is_value_unsigned
+        if not bool_value_storage:
+            continue
+        else:
+            circumference = 4 * side_1
+            print(f"The circumference is {circumference:.2f}")
+
+            surface_area = pow(side_1, 2)
+            print(f"The surface area is {surface_area:.2f}")
+            is_value_valid = True
+
+    return
+
+
+def menu():
+    """
+    Print a menu for user to select which geometric pattern to use in calculations.
+    """
+    while True:
+        answer = input("Enter the pattern's first letter or (q)uit: ")
+
+        if answer == "s":
+            count_values_for_a_square()
+
+        elif answer == "r":
+            # Replace this comment and "pass" with your function calls dealing
+            # with rectangle.
+            pass
+
+        elif answer == "q":
+            return
+
+        else:
+            print("Incorrect entry, try again!")
+
+        # Empty row for the sake of readability.
+        print()
+
+
+def main():
+    menu()
+    print("Goodbye!")
+
+
+if __name__ == "__main__":
+    main()
