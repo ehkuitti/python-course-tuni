8c6f5e96a038536fa4b1a55e6917c6f7091149fa
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-06 17:51:38 +0300

    PARANNETTU RUUDUN TULOSTUS TOIMII

--- pythonProject/Kierros 4/paranneltu_ruudun_tulostus.py
+++ pythonProject/Kierros 4/paranneltu_ruudun_tulostus.py
@@ -4,39 +4,60 @@ Student Id: 150541820
 Name:   Eetu Kuittinen
 Email:  eetu.kuittinen@tuni.fi
 
-TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+Tulostaa neliön oletusarvoilla tai annetuilla parametreilla.
 """
 
+# TODO: the definition of print_box goes here unless it goes after main.
 
 def print_box(width=1, height=1, border_mark="#", inner_mark=' '):
-    i = 1
-    i_sisainen = 1
-    j = 1
-    j_sisainen = 1
+    """ Jos parametrejä ei anneta, tulostaa ruudun em. oletusarvoilla."""
 
-    for i in range(1, height + 1):
+    # Muuttujien alustukset KÄYTTÖJÄRJESTYKSESSÄ
+    rivi = 1
+    sijainti_poikkeusrivilla = 1
+    taydet_rivit_1_ja_vika = 1
 
-        if i == 1 or i == height:
-            for i_sisainen in range(1, width+1):
+    # Huolehtii riveistä
+    for rivi in range(1, height + 1):
+
+        # Ensimmäisen ja viimeisen rivin poikkeustapaukset, joissa tulostetaan
+        # aina täysi määrä border_markkeja
+        if rivi == 1 or rivi == height:
+
+            # Täytetään rivit pelkällä border_markilla
+            for taydet_rivit_1_ja_vika in range(1, width + 1):
                 print(border_mark, sep="", end="")
 
+            # Rivinvaihto for-loopin jälkeen
             print("")
 
-        if j == 1 or j == height-1:
-            for j_sisainen in range(1, width):
-                print(border_mark, sep="", end="")
-                if j_sisainen == 2 or j_sisainen == (width - 1):
-                    print(inner_mark, sep="", end="")
-                else:
+        # Poikkeusrivit
+        else:
+            for sijainti_poikkeusrivilla in range(1, width+1):
+
+                # Alussa ja lopussa tulostetaan border_mark
+                if sijainti_poikkeusrivilla == 1:
                     print(border_mark, sep="", end="")
-            print("")
-            
+
+                elif sijainti_poikkeusrivilla == width:
+                    print(border_mark, sep="")
+                # Muuten tulostetaan inner_mark
+                else:
+                    print(inner_mark, sep="", end="")
+
+            sijainti_poikkeusrivilla = 1
+
+    # Tyhjä rivi
+    print("")
+
 
 def main():
     print_box(5, 4)
     print_box(3, 8, "*")
     print_box(5, 4, "O", "o")
     print_box(inner_mark=".", border_mark="O", height=4, width=6)
+
+# TODO: the definition of print_box could also go here, it is up to you.
 
 
 if __name__ == "__main__":
