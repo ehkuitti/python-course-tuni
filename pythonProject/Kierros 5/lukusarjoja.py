2bd70771fcc3bb77c1edcb11fd2bc292d51dfe14
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-08 10:35:05 +0300

    Kierros 5: lukusarjoja.py valmis

---
+++ pythonProject/Kierros 5/lukusarjoja.py
@@ -0,0 +1,24 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+"""
+
+
+def main():
+
+    i = 0
+    j = 0
+
+    for i in range (0, 101, 2):
+        print(i)
+
+    for j in range (100, -1, -2):
+        print(j)
+
+
+if __name__ == "__main__":
+    main()
