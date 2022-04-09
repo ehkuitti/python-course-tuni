e86f8b54b6840faf268a87e9f9b53a4ff557c0ca
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-09 16:54:31 +0300

    Update rubiikinkuutio

--- pythonProject/Kierros 5/rubikinkuutio.py
+++ pythonProject/Kierros 5/rubikinkuutio.py
@@ -10,17 +10,37 @@ keskiarvoja.
 
 
 def main():
-
+    arvo = 0.00
     i = 1
     lista = []
-    arvo = 0.00
+    indeksi_arvolla_0 = 0
+    indeksi_arvolla_miinus1 = 0
+    keskiarvo = 0
 
     while i < 6:
-        arvo = float(input("Enter the time for performance", i, ": "))
+        print("Enter the time for performance ", i, ": ", sep="", end="")
+        arvo = float(input(""))
         lista.append(arvo)
         i += 1
 
+    # Poistaa duplikaattiarvot listalta
+    lista = list(set(lista))
 
+    # Järjestää arvot nousevaan järjestykseen
+    lista.sort()
+
+    # print("Lista ennen alkioiden poistoa: ", lista)
+
+    # Määritellään muuttujat arvoiltaan nimiään vastaaviksi
+    del lista[0]
+    del lista[-1]
+
+    # print("Lista alkioiden poiston jäljeen: ", lista)
+
+    # Lasketaan keskiarvo listan alkioille
+    keskiarvo = sum(lista) / len(lista)
+
+    print(f"The official competition score is {keskiarvo:.2f} seconds.")
 
 
 if __name__ == "__main__":
