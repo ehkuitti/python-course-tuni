dc660a02996dc95202b84598003ee45ffb59b561
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-10 21:16:57 +0300

    Added rot13_a.py

---
+++ pythonProject/Kierros 6/rot13_a.py
@@ -0,0 +1,38 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+"""
+
+def encrypt(text):
+    """
+    Encrypts its parameter using ROT13 encryption technology.
+
+    :param text: str,  string to be encrypted
+    :return: str, <text> parameter encrypted using ROT13
+    """
+
+    regular_chars   = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k",
+                       "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+                       "w", "x", "y", "z"]
+
+    encrypted_chars = ["n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+                       "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                       "j", "k", "l", "m"]
+
+    if text in regular_chars:
+        print
+
+
+    return ...the result of encryption...
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
