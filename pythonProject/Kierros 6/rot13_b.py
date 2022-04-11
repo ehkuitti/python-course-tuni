7203f2ff4d8e660030c5b476444e4d1c6137ef51
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-11 11:38:27 +0300

    Create rot13_b.py

---
+++ pythonProject/Kierros 6/rot13_b.py
@@ -0,0 +1,24 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+Ohjelma lukee syötettä ja lisää sitä taulukon jatkoksi niin kauan, kunnes syöte
+on tyhjä merkkijono.
+"""
+
+
+def read_message(text):
+    
+
+
+def main():
+    print("Enter text rows to the message. Quit by entering an empty row.")
+    msg = read_message()
+
+    print("The same, shouting:")
+
+
+if __name__ == "__main__":
+    main()
