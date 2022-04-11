00878d1bac76b21098b6363145d3edbd5da9cdc8
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-11 12:19:19 +0300

    Update rot13_b.py

--- pythonProject/Kierros 6/rot13_b.py
+++ pythonProject/Kierros 6/rot13_b.py
@@ -9,15 +9,36 @@ on tyhjä merkkijono.
 """
 
 
-def read_message(text):
-    
+def read_message():
+
+    inputti = ""
+    msg = []
+    is_input_valid = True
+
+    while is_input_valid:
+        inputti = input()
+        if inputti == "" or inputti == " ":
+            is_input_valid = False
+        msg += inputti
+
+    return msg
 
 
 def main():
+
+    msg_shouting = ""
+    msg_lenght = 0
+
     print("Enter text rows to the message. Quit by entering an empty row.")
     msg = read_message()
 
+    msg_lenght = len(msg)
+
+    for i in range (0, msg_lenght, 1):
+        msg_shouting += msg.upper([i])
+
     print("The same, shouting:")
+    print(msg_shouting)
 
 
 if __name__ == "__main__":
