700b851605912e1972c9a0d23816b05d40b22984
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-11 21:39:11 +0300

    Luotu järjestetty_alimerkkijono.py

---
+++ pythonProject/Kierros 6/jarjestetty_alimerkkijono.py
@@ -0,0 +1,23 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+"""
+
+
+def longest_substring_in_order():
+    pass
+
+
+def main():
+
+    merkkijono = ""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
