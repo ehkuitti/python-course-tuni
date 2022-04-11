ad216cd471a532bd353b1e2a34e1b67a2d817ea5
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-11 11:33:35 +0300

    rot13_a_py valmis

--- pythonProject/Kierros 6/rot13_a.py
+++ pythonProject/Kierros 6/rot13_a.py
@@ -7,11 +7,26 @@ Email:  eetu.kuittinen@tuni.fi
 TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
 """
 
-def encrypt(text):
+
+def row_encryption(text):
+    """Tämä funktio käy läpi käyttäjän syöttämää tekstiä ja salaa sitä
+    käyttäen omatekemää encrypt-funktiota. Palauttaa salatun tekstin."""
+    substr = ""
+    text_lenght = 0
+
+    text_lenght = len(text)
+
+    for i in range(0, text_lenght, 1):
+        substr += encrypt(text[i])
+
+    return substr
+
+
+def encrypt(char):
     """
     Encrypts its parameter using ROT13 encryption technology.
 
-    :param text: str,  string to be encrypted
+    :param char: str,  string to be encrypted
     :return: str, <text> parameter encrypted using ROT13
     """
 
@@ -31,17 +46,17 @@ def encrypt(text):
 
     len_encr = len(encrypted_chars)
 
-    if text.isupper():
-        lowercase_text = text.lower()
-        text = lowercase_text
+    if char.isupper():
+        lowercase_text = char.lower()
+        char = lowercase_text
         was_text_uppercase = True
 
-    if text in regular_chars:
-        char_index = regular_chars.index(text)
+    if char in regular_chars:
+        char_index = regular_chars.index(char)
         # print("Char index: ", char_index)
 
     else:
-        return text
+        return char
 
     # Jos taulukon koko ei ole suurempi kuin merkin indeksi
     if char_index <= len_encr:
@@ -59,11 +74,9 @@ def encrypt(text):
 def main():
 
     text = ""
-    text = input("Input text: ")
-    paluuarvo = 0
+    text = input("Input text to be encrypted: ")
 
-    paluuarvo = encrypt(text)
-    print(paluuarvo)
+    row_encryption(text)
 
 
 if __name__ == "__main__":
