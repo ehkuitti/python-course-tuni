8ab31f3f2a8aec2443195c06fb3d764e74b77dab
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-12 17:05:33 +0300

    Keskiarvo ja mediaani valmiita

--- pythonProject/Kierros 6/saatilasto.py
+++ pythonProject/Kierros 6/saatilasto.py
@@ -8,8 +8,43 @@ TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
 """
 
 
-def laske_keskiarvo(paivat, paivien_maara):
+def onko_arvo_pariton(arvo):
+    return arvo % 2 > 0
 
+
+def laske_mediaani(paivat, paivien_maara):
+    # MUUTTUJIEN ALUSTUKSET (AAKKOSJÄRJESTYKSESSÄ)
+    keskimmaisten_keskiarvo = 0
+    mediaani = 0
+    parillinen_keskimmainen_arvo_1 = 0
+    parillinen_keskimmainen_arvo_2 = 0
+    pariton_keskimmainen_arvo = 0
+
+    # Taulukko järjestetään pienimmästä suurimpaan mediaanin selvittämiseksi
+    paivat.sort()
+
+    # Pariton määrä arvoja
+    if onko_arvo_pariton(paivien_maara):
+
+        # Keskimmainen sijaitsee taulukon puolivälissä
+        pariton_keskimmainen_arvo = (paivien_maara // 2)
+        mediaani = paivat[pariton_keskimmainen_arvo]
+
+    # Parillinen määrä arvoja
+    else:
+
+        # Otetaan muuttujiin talteen kaksi keskimmäistä arvoa sekä niiden
+        # keskiarvo
+        pariton_keskimmainen_arvo_1 = (paivien_maara // 2) - 1
+        pariton_keskimmainen_arvo_2 = (paivien_maara // 2)
+        keskimmaisten_keskiarvo = (paivat[pariton_keskimmainen_arvo_1] +
+                                   paivat[pariton_keskimmainen_arvo_2]) / 2
+        mediaani = keskimmaisten_keskiarvo
+
+    print(f"Temperature median: {mediaani:.1f}C")
+
+
+def laske_keskiarvo(paivat, paivien_maara):
     # MUUTTUJIEN ALUSTUKSET (AAKKOSJÄRJESTYKSESSÄ)
     alkiot_yhteensa = 0
     keskiarvo = 0
@@ -19,7 +54,7 @@ def laske_keskiarvo(paivat, paivien_maara):
         alkiot_yhteensa += paivat[indeksi]
 
     keskiarvo = alkiot_yhteensa / paivien_maara
-    print(f"Keskiarvo on: {keskiarvo:.2f}")
+    print(f"Temperature mean: {keskiarvo:.1f}C")
 
 
 def onko_arvo_negatiivinen_tai_nolla(arvo):
@@ -34,18 +69,19 @@ def kay_paivat_lapi(paivien_maara):
     # MUUTTUJIEN ALUSTUKSET (AAKKOSJÄRJESTYKSESSÄ)
     nykyinen_paiva = 0
     paivat = []
+    tyhja_taulukko = []
 
     # Jos käyttäjä syöttää 0 tai vähemmän päiviä, ohjelman suoritus keskeytyy
+    # mainissa.
     if onko_arvo_negatiivinen_tai_nolla(paivien_maara):
-        return
+        return 0, 0
 
-    for paiva in range(1, paivien_maara+1, 1):
+    for paiva in range(1, paivien_maara + 1, 1):
         print("Enter day ", paiva, ". temperature in Celsius: ",
               sep="", end="")
         nykyinen_paiva = float(input())
         paivat.append(nykyinen_paiva)
 
-    # laske_keskiarvo(paivat, paivien_maara)
     return paivat, paivien_maara
 
     # print("Päästiin tänne for-loopin jälkeen")
@@ -53,18 +89,31 @@ def kay_paivat_lapi(paivien_maara):
 
 
 def main():
+    """Mainin tehtävänä on huolehtia kaikkien muiden funktioiden kutsumisesta.
+    Jokainen yksittäinen toiminto on oma funktionsa, jotta sitä voidaan
+    kutsua """
 
+    # MUUTTUJIEN ALUSTUKSET (AAKKOSJÄRJESTYKSESSÄ)
     paivat = []
     paivien_maara = 0
     tiedot_paivista = []
 
     paivien_maara = int(input("Enter amount of days: "))
-    tiedot_paivista[0:1] = kay_paivat_lapi(paivien_maara)
 
+    # Funktion paluuarvot otetaan talteen taulukkoon
+    tiedot_paivista = kay_paivat_lapi(paivien_maara)
+
+    # Jos taulukon 1. ja 2. indeksi ovat nollia, ohjelma suljetaan
+    if tiedot_paivista[0] == 0 and tiedot_paivista[1] == 0:
+        return
+
+    # Mainin muuttujat päivitetään ajan tasalle käy_päivät_läpi funktion pa-
+    # lauttamien arvojen perusteella
     paivat = tiedot_paivista[0]
     paivien_maara = tiedot_paivista[1]
 
     laske_keskiarvo(paivat, paivien_maara)
+    laske_mediaani(paivat, paivien_maara)
 
 
 if __name__ == "__main__":
