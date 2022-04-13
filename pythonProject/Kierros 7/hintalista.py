4e0bfc212078046bc3e81f2726c9042f1cf23a6d
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-13 11:26:21 +0300

    Create hintalista.py

---
+++ pythonProject/Kierros 7/hintalista.py
@@ -0,0 +1,29 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+"""
+
+
+PRICES = {
+    "milk": 1.09, "fish": 4.56, "bread": 2.10,
+    "chocolate": 2.7, "grasshopper": 13.25,
+    "sushi": 19.9, "noodles": 0.97, "beans": 0.87,
+    "bananas": 1.05, "Pepsi": 3.15,  "pizza": 4.15,
+}
+
+
+def main():
+
+    syote = ""
+    syote = input("Enter product name: ")
+
+    if syote == "" or syote == " " or syote == "  ":
+        print("Bye!")
+
+
+if __name__ == "__main__":
+    main()
