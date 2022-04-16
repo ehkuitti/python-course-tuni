9e395653479fd125372d1bf571c3d56a7048c0b6
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-16 20:20:10 +0300

    Ohjelman lakkauttaminen tyhjällä syötteellä toimii

---
+++ pythonProject/Kierros 7/sanatiheyslaskuri.py
@@ -0,0 +1,24 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 150541820
+Name:   Eetu Kuittinen
+Email:  eetu.kuittinen@tuni.fi
+
+TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
+"""
+
+
+def main():
+    syote = ""
+
+    print("Enter rows of text for word counting. Empty row to quit.")
+    syote = input()
+
+    while syote != "":
+        syote = input()
+
+    print("Huhhu, morojesh!")
+
+
+if __name__ == "__main__":
+    main()
