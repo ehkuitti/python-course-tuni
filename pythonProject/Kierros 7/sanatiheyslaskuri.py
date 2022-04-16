9d4cc2ba09573bddd952345dc066449531355cc2
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-16 20:43:34 +0300

    Update sanatiheyslaskuri.py

--- pythonProject/Kierros 7/sanatiheyslaskuri.py
+++ pythonProject/Kierros 7/sanatiheyslaskuri.py
@@ -9,15 +9,31 @@ TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
 
 
 def main():
+    i = 0
+    input_taulukko = []
+    sanakirja_tulostaulu = {}
     syote = ""
+    syote_pienilla_kirjaimilla = ""
+    splitattu_pienten_kirjainten_syote = ""
 
     print("Enter rows of text for word counting. Empty row to quit.")
     syote = input()
 
     while syote != "":
         syote = input()
+        syote_pienilla_kirjaimilla = syote.lower()
+        splitattu_pienten_kirjainten_syote = syote_pienilla_kirjaimilla.split()
+        input_taulukko.append(splitattu_pienten_kirjainten_syote)
 
-    print("Huhhu, morojesh!")
+    while i < len(input_taulukko):
+
+        if input_taulukko[i] not in sanakirja_tulostaulu:
+            sanakirja_tulostaulu[input_taulukko[i]] = \
+                input_taulukko[i]
+
+        i += 1
+
+    print("Tulostaulu: ", sanakirja_tulostaulu)
 
 
 if __name__ == "__main__":
