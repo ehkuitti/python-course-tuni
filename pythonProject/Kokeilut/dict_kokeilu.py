68927605a6d0ad940d1ff7a9933a768db619bb21
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-17 20:37:01 +0300

    Sassian määrälaskuri toimii

--- pythonProject/Kokeilut/dict_kokeilu.py
+++ pythonProject/Kokeilut/dict_kokeilu.py
@@ -13,18 +13,23 @@ def main():
     my_input = ""
     my_input_splitted = ""
     my_list = []
+    my_dict = {}
     amount_of_a_letter = 0
 
-    my_input = input("Enter a word: ")
+    my_input = input("Enter some words: ")
+
     my_input_splitted = my_input.split()
     for i in range(0, len(my_input_splitted), 1):
         my_list.append(my_input_splitted[i])
 
-    print(my_list)
-    print("Sassian määrä: ")
+    print("My list: ", my_list)
     amount_of_a_letter = my_list.count("sassia")
-    print(amount_of_a_letter)
+    print("Amount of sassia: ", amount_of_a_letter)
 
+    i = 0
+
+    my_dict[my_list[i]] = amount_of_a_letter
+    print(my_dict)
 
 
 
