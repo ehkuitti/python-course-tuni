d9ed9c7efdbde795737b7f1882de4dc45ad6a982
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-04-20 11:18:46 +0300

    Update sanatiheyslaskuri.py

--- pythonProject/Kierros 7/sanatiheyslaskuri.py
+++ pythonProject/Kierros 7/sanatiheyslaskuri.py
@@ -8,60 +8,59 @@ TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
 """
 
 
-def count_amount_of_a_word():
-    """Funktion tarkoituskena on tallentaa sanoja taulukkoon ja laskea niiden
-    määriä."""
-    amount_of_found_words = 0
-    lenght_of_my_split_input = ""
+def main():
+
     my_input = ""
-    my_list = []
-    my_lowercase_input = ""
-    my_split_input = ""
-    my_dict = {}
-    is_input_valid = True
-    word_i_count = 0
+    filename = ""
+    dictionary = {}
+    split_my_input = []
+    length_of_my_input = 0
+    length_of_split_my_input = 0
 
     print("Enter rows of text for word counting. Empty row to quit.")
 
-    # Syötteen keräämistä jatketaan niin kauan, kunnes syötetään pelkkä enter
-    while is_input_valid:
+    while True:
 
         my_input = input()
 
+        # Luuppi kaadetaan tyhjällä merkkijonolla, käytännössä pelkän enterin
+        # syöttämisellä
         if my_input == "":
-            is_input_valid = False
+            break
 
-        else:
-            my_lowercase_input = my_input.lower()
-            my_split_input \
-                = my_lowercase_input.split()
+        length_of_my_input = len(my_input)
 
-        lenght_of_my_split_input = len(my_split_input)
-        i_value_in_memory = 0
+        for i in range(0, length_of_my_input, 1):
 
-        for i in range(0, lenght_of_my_split_input, 1):
+            split_my_input = my_input.split()
+            length_of_split_my_input = len(my_input)
 
-            # Lisää arvoja taulukkoon
-            my_list.append(my_split_input[i])
-            # print("My list in for loop: ", my_list)
+            # index_1_as_int = int(split_sentence[1])
 
-        i = 0
-        while i < lenght_of_my_split_input:
-            word_i_count = my_list.count(my_split_input[i])
-            # print("My i word count: ", word_i_count)
-            i += 1
+            for j in range(0, length_of_split_my_input, 1):
 
-        my_dict[my_split_input[i-1]] = word_i_count
+                if split_my_input[j] in dictionary:
+                    dictionary[split_my_input[j]] = dictionary.get(
+                        split_my_input[j]) + split_my_input[j].count(
+                        split_my_input[j])
 
-    print("My dict: ", my_dict)
-    print("My list: ", my_list)
-    # print("Sassia count: ", my_list.count("sassia"))
+                else:
+                    dictionary[split_my_input[j]] = split_my_input[j].count(
+                        split_my_input[j])
 
+            # print("Sanakirjan sisältö rivillä 33: ", dictionary)
+    
+        # print("Sortattu dictionary: ", sorted_dictionary)
+    
+        # sorted_dictionary = dict(sorted(dictionary.items()))
+        print("Haloust world!")
 
-def main():
-
-    count_amount_of_a_word()
-
+        # print("Contestant score: ")
+        for key, value in sorted_dictionary.items():
+            print(key, value)
+    
 
 if __name__ == "__main__":
     main()
+
+# print("Arvot splitatussa taulukossa: ", split_file_line)
