15141ea486c318bd60dbd3aebf51b1a24ce86bbf
ehkuitti <eetu.kuittinen@student.laurea.fi>
2022-04-20 20:34:35 +0300

    SERIES.PY VALMISgit add -A!

--- pythonProject/Kierros 8/series.py
+++ pythonProject/Kierros 8/series.py
@@ -9,56 +9,88 @@ TÄHÄN TULEE KUVAUS SIITÄ, MITÄ KOODITIEDOSTON OLISI TARKOITUS TEHDÄ.
 
 
 def main():
+    # MUUTTUJIEN ALUSTUKSET (MUUTTUJATYYPEITTÄIN)
 
-    sarjat = []
-    sarjan_genret = ""
-    sarjan_genret_splitattuna = []
-    sarjan_genret_splitattuna_pituus = 0
+    # KOKONAISLUVUT
+    int_genret_splitattuna_pituus = 0
+
+    # LISTAT
+    lst_sarjat_genreineen = []
+    lst_genret_splitattuna = []
+
+    # MERKKIJONOT
+    str_tiedostonimi = ""
+    str_stripattu_nimi = ""
+    str_stripatut_genret = ""
+    str_syote = ""
+    genre = ""
+
+    # SANAKIRJAT
+    dict_sarjat_genreineen = {}
+
+    # SETIT
     genresetti = set()
-    genresetti_aakkosjarjestyksessa = set()
-    genresetti_aakkosjarjestyksessa_merkkijonona = ""
-    tiedostonimi = ""
 
-    tiedostonimi = input("Enter the name of the file: ")
+    str_tiedostonimi = input("Enter the name of the file: ")
 
     try:
-        tiedosto = open(tiedostonimi, mode="r")
+        tiedosto = open(str_tiedostonimi, mode="r")
 
     except OSError:
-        print(f"Error: opening the file '{tiedostonimi}' failed!")
+        print(f"Error: opening the file '{str_tiedostonimi}' failed!")
         return
 
-    print("Available genres are: ", sep="", end="")
-
-    i = 0
     for rivi in tiedosto:
 
-        # Splitataan tiedosto puolipisteen kohdalta
-        sarjat = rivi.split(";")
+        # Splitataan tiedosto puolipisteen kohdalta: Indeksiin 0 tulee sarjan
+        # nimi, indeksiin 1 genret.
+        lst_sarjat_genreineen = rivi.split(";")
 
-        # Sajran genret sijaitsevat splitin jälkeen indeksissä 1. Lopun
-        # .stripillä leikataan rivinvaihto pois syötteestä.
-        sarjan_genret = sarjat[1].strip()
+        # Poistaa rivinvaihdon rivin lopusta
+        str_stripattu_nimi = lst_sarjat_genreineen[0].strip()
+        str_stripatut_genret = lst_sarjat_genreineen[1].strip()
 
-        # Genret splitataan, jotta niitä voidaan käsitellä
-        sarjan_genret_splitattuna = sarjan_genret.split(",")
-        sarjan_genret_splitattuna_pituus = len(sarjan_genret_splitattuna)
+        # Splitataan genret pilkun kohdalta .splitillä, palauttaa listan.
+        lst_genret_splitattuna = str_stripatut_genret.split(",")
+        int_genret_splitattuna_pituus = len(lst_genret_splitattuna)
 
-        # Lisätään indeksistä i löytyvä genre genresettiin
-        for i in range(0, sarjan_genret_splitattuna_pituus, 1):
-            genresetti.add(sarjan_genret_splitattuna[i])
-            # print(i, ":", genresetti)
+        # Lisätään nimi ja taulukko (lista) parina sanakirjaan
+        if str_stripattu_nimi not in dict_sarjat_genreineen:
+            dict_sarjat_genreineen[str_stripattu_nimi] = lst_genret_splitattuna
+
+        # Tehdään setti valmiiksi alun tulostusta varten
+        for i in range(0, int_genret_splitattuna_pituus, 1):
+            genresetti.add(lst_genret_splitattuna[i])
 
     genresetti_aakkosjarjestyksessa = sorted(genresetti)
-    genresetti_aakkosjarjestyksessa_merkkijonona = ", ".join\
+    str_genresetti_aakkosjarjestyksessa = ", ".join \
         (genresetti_aakkosjarjestyksessa)
 
-    print(genresetti_aakkosjarjestyksessa_merkkijonona)
+    print("Available genres are: ", sep="", end="")
+    print(str_genresetti_aakkosjarjestyksessa)
 
-    # for arvo in genresetti_aakkosjarjestyksessa:
-    #     print(arvo, ", ", sep="", end="")
+    while True:
+        print("> ", sep="", end="")
+        str_syote = input()
 
-    # print(sarjan_genret_splitattuna)
+        if str_syote == "exit":
+            break
+
+        else:
+            for sarja in sorted(dict_sarjat_genreineen):
+                for genre in sorted(dict_sarjat_genreineen[sarja]):
+                    if genre == str_syote:
+                        print(sarja)
+
+
+                # if genre == str_syote:
+                #     # for genre in sorted(dict_sarjat_genreineen[sarja]):
+                #     print(sarja)
+
+
+                # print(dict_sarjat_genreineen)
+
+        # print(dict_sarjat_genreineen)
 
 
 if __name__ == "__main__":
