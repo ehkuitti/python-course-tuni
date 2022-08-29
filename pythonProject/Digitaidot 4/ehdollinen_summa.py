fc6eb068440312f15167c905fba33e6777ad00c1
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-08-29 15:42:26 +0300

    CS014 Ehtolauseita

---
+++ pythonProject/Digitaidot 4/ehdollinen_summa.py
@@ -0,0 +1,14 @@
+print("Anna luku 1:")
+luku1 = int(input())
+
+print("Anna luku 2:")
+luku2 = int(input())
+
+summa = luku1 + luku2
+
+if summa > 10:
+    print("yli 10")
+elif summa == 10:
+    print("tasan 10")
+else:
+    print("alle 10")
