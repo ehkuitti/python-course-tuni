8c2e72ed30c03869692040ce59966f4d7dd1315e
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2022-08-29 15:36:36 +0300

    COMP.CS.014-kurssi aloitettu

---
+++ pythonProject/Digitaidot 4/keskiarvo.py
@@ -0,0 +1,12 @@
+print("Anna luku 1:")
+luku1 = float(input())
+
+print("Anna luku 2:")
+luku2 = float(input())
+
+print("Anna luku 3:")
+luku3 = float(input())
+
+print("Keskiarvo on:")
+keskiarvo = (luku1 + luku2 + luku3) / 3
+print(keskiarvo)
