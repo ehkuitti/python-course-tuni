832158ef3c388e027a18d14b769851aea76eff1c
ehkuitti <72074501+ehkuitti@users.noreply.github.com>
2023-06-25 17:51:53 +0300

    Aloita 1. GUI-tehtävä counter.py

---
+++ pythonProject/Omat (ei kurssimateriaalia)/Bulgaria-laskin.py
@@ -0,0 +1,4 @@
+norwegian_lennot_menopaluu = 238
+secrets_sunny_beach_hotelli_viikko_all_incl = 1508
+
+print((norwegian_lennot_menopaluu + secrets_sunny_beach_hotelli_viikko_all_incl) / 2)
---
+++ pythonProject/Omat (ei kurssimateriaalia)/RockImPark-laskin.py
@@ -0,0 +1,12 @@
+menopaluu_lennot_berliini = 169
+menopaluu_juna_berliini_nuremberg = 36 * 2
+paasylippu_festareille = 268 # 3 päivää
+# hotelli_3_yota_nuremberg = 257
+
+print("KULUT")
+print()
+print(f"Lennot menopaluu: {menopaluu_lennot_berliini}€")
+print(f"Juna Berliini-Nürnberg: {menopaluu_juna_berliini_nuremberg}€")
+print(f"Festarien pääsyliput (3pv): {paasylippu_festareille}€")
+print()
+print(f"{menopaluu_lennot_berliini + menopaluu_juna_berliini_nuremberg + paasylippu_festareille}€")
